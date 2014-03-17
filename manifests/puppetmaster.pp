1a3aaa8c50423be250099bd704a1bbb1c7ba47a6
groob <vrancean+github@gmail.com>
2014-03-17 12:34:57 -0400

    removed PE settings

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -4,39 +4,39 @@
 # capabilities.
 #
 class role::puppetmaster (
-  $r10k_environments_dir    = '/etc/puppetlabs/puppet/environments',
-  $r10k_environments_remote = 'https://github.com/puppetlabs-seteam/puppet-environments',
+  $r10k_environments_dir    = '/etc/puppet/environments',
+  $r10k_environments_remote = 'https://github.com/groob/puppet-environments',
   $srv_root                 = '/var/seteam-files',
 ) {
   # Custom PE Console configuration
-  include console_env
+  # include console_env
   include git
-  include apache
+  # include apache
 
   # Puppet master firewall rules
-  include profile::firewall
-  Firewall {
-    require => Class['profile::firewall::pre'],
-    before  => Class['profile::firewall::post'],
-    chain   => 'INPUT',
-    proto   => 'tcp',
-    action  => 'accept',
-  }
-  firewall { '110 puppetmaster allow all': dport  => '8140';  }
-  firewall { '110 dashboard allow all':    dport  => '443';   }
-  firewall { '110 mcollective allow all':  dport  => '61613'; }
-  firewall { '110 apache allow all':       dport  => '80';    }
+  # include profile::firewall
+  # Firewall {
+  #   require => Class['profile::firewall::pre'],
+  #   before  => Class['profile::firewall::post'],
+  #   chain   => 'INPUT',
+  #   proto   => 'tcp',
+  #   action  => 'accept',
+  # }
+  # firewall { '110 puppetmaster allow all': dport  => '8140';  }
+  # firewall { '110 dashboard allow all':    dport  => '443';   }
+  # firewall { '110 mcollective allow all':  dport  => '61613'; }
+  # firewall { '110 apache allow all':       dport  => '80';    }
 
-  apache::vhost { 'seteam-files':
-    vhost_name => '*',
-    port       => '80',
-    docroot    => $srv_root,
-    priority   => '10',
-  }
+  # apache::vhost { 'seteam-files':
+  #   vhost_name => '*',
+  #   port       => '80',
+  #   docroot    => $srv_root,
+  #   priority   => '10',
+  # }
 
   package { 'r10k':
     ensure   => present,
-    provider => pe_gem,
+    provider => gem,
   }
 
   # Template uses:
@@ -52,21 +52,21 @@ class role::puppetmaster (
 
   ini_setting { 'puppet_modulepath':
     ensure  => present,
-    path    => '/etc/puppetlabs/puppet/puppet.conf',
+    path    => '/etc/puppet/puppet.conf',
     section => 'main',
     setting => 'modulepath',
     value   => "${r10k_environments_dir}/\$environment/modules:/opt/puppet/share/puppet/modules",
   }
   ini_setting { 'puppet_manifest':
     ensure  => present,
-    path    => '/etc/puppetlabs/puppet/puppet.conf',
+    path    => '/etc/puppet/puppet.conf',
     section => 'main',
     setting => 'manifest',
     value   => "${r10k_environments_dir}/\$environment/manifests/site.pp"
   }
   ini_setting { 'puppet_hieraconfig':
     ensure  => present,
-    path    => '/etc/puppetlabs/puppet/puppet.conf',
+    path    => '/etc/puppet/puppet.conf',
     section => 'main',
     setting => 'hiera_config',
     value   => "${r10k_environments_dir}/production/hiera.yaml"
