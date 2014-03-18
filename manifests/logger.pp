a90d2a7ea9def119146f457088eb7893e404dd5b
groob <vrancean+github@gmail.com>
2014-03-18 19:48:56 -0400

    added firewall rules

--- manifests/logger.pp
+++ manifests/logger.pp
@@ -6,5 +6,14 @@ class role::logger {
   include profile::logstash
   include profile::elasticsearch
   include profile::kibana
+  include profile::firewall
+  Firewall {
+    require => Class['profile::firewall::pre'],
+    before  => Class['profile::firewall::post'],
+    chain   => 'INPUT',
+    proto   => 'tcp',
+    action  => 'accept',
+  }
+  firewall { '110 apache allow all':       dport  => '80';    }
 }
 
