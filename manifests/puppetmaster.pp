1f4fc5f2aadd657c78a502f4d63ad07e458ad3df
groob <vrancean+github@gmail.com>
2014-07-13 00:54:16 -0400

    removed old config

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -15,22 +15,8 @@ class role::puppetmaster (
     listen_address      => '0.0.0.0',
     ssl_listen_address  => '0.0.0.0',
   }
-  class { 'puppetdb::master::config': }
-
-  package { 'r10k':
-    ensure   => present,
-    provider => gem,
-  }
-
-  # Template uses:
-  #   - $r10k_environments_dir
-  #   - $r10k_environments_remote
-  file { '/etc/r10k.yaml':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('role/puppetmaster/r10k.yaml.erb'),
+  class { 'puppetdb::master::config':
+    puppet_service_name => 'apache2',
   }
 
   file { '/etc/puppet/hiera.yaml':
@@ -41,40 +27,12 @@ class role::puppetmaster (
     content => template('role/puppetmaster/hiera.yaml.erb'),
   }
 
-  ini_setting { 'puppet_modulepath':
-    ensure  => present,
-    path    => '/etc/puppet/puppet.conf',
-    section => 'main',
-    setting => 'modulepath',
-    value   => "${r10k_environments_dir}/\$environment/modules:/opt/puppet/share/puppet/modules",
-  }
-  ini_setting { 'puppet_manifest':
-    ensure  => present,
-    path    => '/etc/puppet/puppet.conf',
-    section => 'main',
-    setting => 'manifest',
-    value   => "${r10k_environments_dir}/\$environment/manifests/site.pp"
-  }
   ini_setting { 'puppet_hieraconfig':
     ensure  => present,
     path    => '/etc/puppet/puppet.conf',
     section => 'main',
     setting => 'hiera_config',
     value   => '/etc/puppet/hiera.yaml',
-  }
-
-  Exec {
-    path    => '/opt/puppet/bin:/usr/bin:/bin',
-    require => [
-      Package['r10k'],
-      Class['git'],
-      File['/etc/r10k.yaml'],
-    ],
-  }
-
-  exec { 'instantiate_environments':
-    command => '/usr/bin/r10k deploy environment',
-    creates => $r10k_environments_dir,
   }
 
 }
